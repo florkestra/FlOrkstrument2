76cc91713753747e0abcba977c6f6956b1d44e47
Adam Tindale <adam@adamtindale.com>
2014-07-15 15:52:11 -0400

    midi working in browser, not app

--- synth.js
+++ synth.js
@@ -2,13 +2,34 @@ var synth = flock.synth({
   synthDef:{
       id: "testy",
       ugen: "flock.ugen.sin",
-      freq: 400, 
-      mul:0.5,
+      mul: {
+        id: "freqy",
+        ugen: "flock.ugen.dust",
+        density: 100,
+        mul: 0.3,
+        add: 0.4
+      }, 
   }
 });
 
-$('#f1').on("change mousemove", function() {
+var dustyfreq = {
+        id: "freqy",
+        ugen: "flock.ugen.dust",
+        density: 100,
+        mul: 0.3,
+        add: 0.4
+      };
+
+$('#f1').on("change", function() {
     synth.set( "testy.freq",$(this).val() * 2 + 200 );
+    console.log("fadfadf");
+});
+
+$('#f2').on("change", function() {
+    synth.set("testy.mul", dustyfreq);
+    var thing = $(this).val()/127;
+    synth.set( "freqy.mul",thing  );
+    synth.set( "freqy.add",1-thing  );
 });
 
 $('input[type="range"]').each( function(index){
@@ -25,17 +46,27 @@ $('#onoff').on("change", function(){
     }
 });
 
+
+function midi2Freq (num){
+  return Math.pow(2, (num-69)/12) * 440;    
+};
+console.log(midi2Freq(60));
+
 var midiConnection = flock.midi.connection({
     openImmediately: true,
-    ports: 0,
+    ports: {manufacturer: "AKAI"},
     listeners: {
         noteOn: function (msg) {
+         $('#midiDisplay').html( fluid.prettyPrintJSON(msg) );
+    	   synth.set("testy.freq", midi2Freq(msg.note+24));
     	   synth.set("testy.mul", 1);
         },
         noteOff: function (msg) {
+         $('#midiDisplay').html( fluid.prettyPrintJSON(msg) );
           synth.set("testy.mul", 0);
         },
         control: function (msg) {
+         $('#midiDisplay').html( fluid.prettyPrintJSON(msg) );
           switch(msg.number){
             case 1:
               $('#f1').val(msg.value).trigger("change");
@@ -44,7 +75,22 @@ var midiConnection = flock.midi.connection({
               $('#f2').val(msg.value).trigger("change");
               break;
             case 3:  
-              $('#f2').val(msg.value).trigger("change");
+              $('#f3').val(msg.value).trigger("change");
+              break;
+            case 4:  
+              $('#f4').val(msg.value).trigger("change");
+              break;
+            case 5:  
+              $('#f5').val(msg.value).trigger("change");
+              break;
+            case 3:  
+              $('#f6').val(msg.value).trigger("change");
+              break;
+            case 7:  
+              $('#f7').val(msg.value).trigger("change");
+              break;
+            case 8:  
+              $('#f8').val(msg.value).trigger("change");
               break;
           }
         }
